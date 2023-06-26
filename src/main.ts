3f3647ef9001661fbbb262d1a3760eb50a3ccc6e
STEVE <ymyd.steve@gmail.com>
2023-06-26 18:32:43 +0900

    fix : domain cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { swaggerSetting } from './util/swagger.util';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3002', 'http://localhost:3001'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     exposedHeaders: ['Set-Cookie'],
     credentials: true,
