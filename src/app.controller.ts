416957ea9f74b043e308ca4013ea0e1c22c2ff75
STEVE <ymyd.steve@gmail.com>
2023-06-26 20:12:12 +0900

    fix : setting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request, Response } from 'express';
+import { SocketOptionsDto } from './socket.dto';
 
 @Controller()
 export class AppController {
@@ -12,16 +13,16 @@ export class AppController {
   }
 
   @Get('setcookie')
-  async setCookiTest(@Res() res: Response, @Req() req: Request) {
+  async setCookiTest(@Query() info: SocketOptionsDto, @Res() res: Response) {
     try {
       res.cookie('Authorization', 'token', {
         // domain: 'vet-hospital.store',
-        domain: 'localhost',
-        sameSite: 'strict',
-        secure: false,
-        httpOnly: true,
+        domain: info.domain || 'localhost',
+        sameSite: info.sameSite || 'strict',
+        secure: info.secure || false,
+        httpOnly: info.httpOnly || true,
       });
-      res.send('');
+      res.send(info);
     } catch (err) {
       throw new Error(err);
     }
